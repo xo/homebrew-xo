49a9926739228b452a24ae3ae1892fa4de434746
Kenneth Shaw <kenneth.shaw@knq.io>
2017-10-27 00:36:52 +0700

    Adding instantclient-sdk formula, and options to usql formula

---
+++ Formula/instantclient-sdk.rb
@@ -0,0 +1,38 @@
+class InstantclientSdk < Formula
+  desc "Oracle Instant Client SDK x64."
+  homepage "http://www.oracle.com/technetwork/topics/intel-macsoft-096467.html"
+  url "https://raw.githubusercontent.com/strongloop/loopback-oracle-builder/master/deps/oracle/MacOSX/x64/instantclient-basiclite-macos.x64-12.1.0.2.0.zip"
+  sha256 "ac7e97661a2bfac69b3262150641914f456c7806ba2a7850669fb83abac120e8"
+
+  depends_on "pkg-config" => :run
+
+  resource "instantclient-sdk" do
+    url "https://raw.githubusercontent.com/strongloop/loopback-oracle-builder/master/deps/oracle/MacOSX/x64/instantclient-sdk-macos.x64-12.1.0.2.0.zip"
+    sha256 "63582d9a2f4afabd7f5e678c39bf9184d51625c61e67372acdbc7b42ed8530ac"
+  end
+
+  def install
+    %w(libclntsh.dylib libocci.dylib).each do |dylib|
+      ln_s "#{dylib}.12.1", dylib
+    end
+    lib.install Dir["*.dylib*"]
+
+    resource("instantclient-sdk").stage do
+      lib.install ["sdk"]
+    end
+
+    (lib+"pkgconfig/oci8.pc").write pc_file
+  end
+
+  def pc_file; <<~EOS
+    prefix=#{opt_prefix}
+
+    Name: OCI
+    Description: Oracle Instant Client
+    Version: #{version}
+    Libs: -L${prefix}/lib -lclntsh
+    Libs.private:
+    Cflags: -I${prefix}/lib/sdk/include
+    EOS
+  end
+end
--- Formula/usql.rb
+++ Formula/usql.rb
@@ -1,29 +1,49 @@
 $ver     = "v0.6.0-rc1"
 $hash    = "dab5524f7f5f96a24baac8cb5281cd33b5e1054d554a6f4647a3bb788752a578"
+$pkg     = "github.com/xo/usql"
 
-$tags    = "most fts5 vtable json1 no_adodb"
-$ldflags = "-s -w -X github.com/xo/usql/text.CommandVersion=#{$ver}"
+$tags    = %w(most fts5 vtable json1 no_adodb)
+$ldflags = "-s -w -X #{$pkg}/text.CommandVersion=#{$ver}"
 
 class Usql < Formula
   desc     "universal command-line SQL client interface"
-  homepage "https://github.com/xo/usql"
-  head     "https://github.com/xo/usql.git"
-  url      "https://github.com/xo/usql/archive/#{$ver}.tar.gz"
+  homepage "https://#{$pkg}"
+  head     "https://#{$pkg}.git"
+  url      "https://#{$pkg}/archive/#{$ver}.tar.gz"
   sha256   $hash
+
+  option "with-oracle", "Build with Oracle database (instantclient) support"
+  option "with-odbc",   "Build with ODBC (unixodbc) support"
 
   depends_on "go"  => :build
   depends_on "dep" => :build
 
+  if build.with? "oracle" then
+    $tags   << "oracle"
+    depends_on "pkg-config"        => :build,
+               "instantclient-sdk" => [:build, :run]
+  end
+
+  if build.with? "odbc" then
+    $tags   << "odbc"
+    depends_on "unixodbc"
+  end
+
   def install
-    ENV["GOPATH"] = buildpath
+    ENV["GOPATH"]     = buildpath
 
-    (buildpath/"src/github.com/xo/usql").install buildpath.children
+    if build.with? "oracle"
+      ENV["PKG_CONFIG"] = "#{Formula["pkg-config"].opt_bin}/pkg-config"
+      ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["instantclient-sdk"].lib}/pkgconfig"
+    end
 
-    cd "src/github.com/xo/usql" do
+    (buildpath/"src/#{$pkg}").install buildpath.children
+
+    cd "src/#{$pkg}" do
       system "dep", "ensure", "--vendor-only"
 
       system "go", "build",
-        "-tags",    $tags,
+        "-tags",    $tags.join(" "),
         "-ldflags", $ldflags,
         "-o",       bin/"usql"
     end
