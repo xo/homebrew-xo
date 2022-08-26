77fab9da4989e937bceeff00b07f541eb91ff845
Kenneth Shaw <ken@usql.app>
2022-08-26 00:15:50 +0000

    Update usql version to v0.12.12\n\nMinor fixes/changes to workflows to enable automated version bumps for the Homebrew and AUR repos.
    **Full Changelog**: https://github.com/xo/usql/compare/v0.12.11...v0.12.12

--- Formula/usql.rb
+++ Formula/usql.rb
@@ -5,8 +5,8 @@ class Usql < Formula
   desc "universal command-line SQL client interface"
   homepage "https://#{$pkg}"
   head "https://#{$pkg}.git"
-  url "https://github.com/xo/usql/archive/v0.12.11.tar.gz"
-  sha256 "f95dacc957acc4cfdf8ae5cfe48ff06fd045ac3ec9b05890b926fb419b412510"
+  url "https://github.com/xo/usql/archive/v0.12.12.tar.gz"
+  sha256 "b1a0671687b15dc1b13d60b35f12a49918c197f0f4d6cf852ff0492b035ac3e0"
 
   option "with-odbc", "Build with ODBC (unixodbc) support"
 
