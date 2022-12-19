b420f3f2d558937404c14e0a90fe6344a137a0db
Kenneth Shaw <ken@usql.app>
2022-12-19 09:06:01 +0000

    Update usql version to v0.13.2
    ## What's Changed
    * Add support for Databend by @everpcpc in https://github.com/xo/usql/pull/381
    
    ## New Contributors
    * @everpcpc made their first contribution in https://github.com/xo/usql/pull/381
    
    **Full Changelog**: https://github.com/xo/usql/compare/v0.13.1...v0.13.2

--- Formula/usql.rb
+++ Formula/usql.rb
@@ -5,8 +5,8 @@ class Usql < Formula
   desc "universal command-line SQL client interface"
   homepage "https://#{$pkg}"
   head "https://#{$pkg}.git"
-  url "https://github.com/xo/usql/archive/v0.13.1.tar.gz"
-  sha256 "ec19e8ce838f11b43acc0ba46cf193b561e894608e7e7addf29936a8e76e9057"
+  url "https://github.com/xo/usql/archive/v0.13.2.tar.gz"
+  sha256 "02ff2b816e1c1d0a5310f9fcb1de3b17642b84048637279e972d5fbf8b87c9a5"
 
   option "with-odbc", "Build with ODBC (unixodbc) support"
 
