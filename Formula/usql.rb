d3c73796b27efb2125fd7594615c4879c732b05a
Kenneth Shaw <ken@usql.app>
2024-09-23 04:52:14 +0700

    Removing sqlite_userauth tag from build

--- Formula/usql.rb
+++ Formula/usql.rb
@@ -1,5 +1,5 @@
 $pkg     = "github.com/xo/usql"
-$tags    = %w(most sqlite_app_armor sqlite_fts5 sqlite_introspect sqlite_json1 sqlite_math_functions sqlite_stat4 sqlite_userauth sqlite_vtable no_adodb)
+$tags    = %w(most sqlite_app_armor sqlite_fts5 sqlite_introspect sqlite_json1 sqlite_math_functions sqlite_stat4 sqlite_vtable no_adodb)
 
 class Usql < Formula
   desc "universal command-line SQL client interface"
