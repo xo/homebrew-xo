2fde7d78e09669276dd43833bb91b358ce642de4
Kenneth Shaw <ken@usql.app>
2025-07-20 01:56:24 +0000

    Update usql version to v0.19.25
    **Full Changelog**: https://github.com/xo/usql/compare/v0.19.24...v0.19.25
    
    * [usql_static-0.19.25-linux-amd64-f2b0aa9b](https://www.virustotal.com/gui/file-analysis/M2IxNTM3MzNlYjM1NWQ5M2FmYjY0M2FkZjRmYjk0ODQ6MTc1Mjk3NTk4MQ==/detection)
    * [usql_static-0.19.25-linux-arm64-f39153a7](https://www.virustotal.com/gui/file-analysis/YTI0YWVlN2U0ZjBlMGY3NGE1YmJkZTY2YjZmNmQ3OGU6MTc1Mjk3NTk4NA==/detection)
    * [usql-0.19.25-darwin-amd64-57a12d05](https://www.virustotal.com/gui/file-analysis/ZTFiN2E1NzY5MzU0ZGJkOTg5NWFjOWM2ZjRlZmJhYzc6MTc1Mjk3NTk4Nw==/detection)
    * [usql-0.19.25-darwin-arm64-26209db6](https://www.virustotal.com/gui/file-analysis/MDVlNTU0NTYzNzg3NmQzZDZmZjFlMmVmZmNhMjJlOTI6MTc1Mjk3NTk5MQ==/detection)
    * [usql-0.19.25-darwin-universal-e5d56989](https://www.virustotal.com/gui/file-analysis/YTMxYWM5MjI4NmE4MWRhYzVlZDkwM2IxY2VkMjY4ZWI6MTc1Mjk3NjA0Mw==/detection)
    * [usql-0.19.25-linux-amd64-d044f563](https://www.virustotal.com/gui/file-analysis/ODgwODRjMTBkMDEyNmI2MjUyZGMyZjMyMjA3ODQzYTc6MTc1Mjk3NjA0Ng==/detection)
    * [usql-0.19.25-linux-arm-0fd19c84](https://www.virustotal.com/gui/file-analysis/NmFjMGFhZGU0ZWZmZGQ5ZmE5MjkwZTMyZTM2YjJjZTI6MTc1Mjk3NjA0OQ==/detection)
    * [usql-0.19.25-linux-arm64-0e68823e](https://www.virustotal.com/gui/file-analysis/NjNiNjlkMWRhNTFmYjNjZTIxYzlkNzMxM2I2NmI2ZWI6MTc1Mjk3NjA1Mg==/detection)

--- Formula/usql.rb
+++ Formula/usql.rb
@@ -5,8 +5,8 @@ class Usql < Formula
   desc "universal command-line SQL client interface"
   homepage "https://#{$pkg}"
   head "https://#{$pkg}.git"
-  url "https://github.com/xo/usql/archive/v0.19.24.tar.gz"
-  sha256 "b4a78bf942754dfb835710533a25a83f52d0cafb3a4caf896a92398886b14b1c"
+  url "https://github.com/xo/usql/archive/v0.19.25.tar.gz"
+  sha256 "5c578e4484799e7007bce5b7de2955c16586ddb4088d2fbf030efee46c75faf0"
 
   option "with-odbc", "Build with ODBC (unixodbc) support"
 
